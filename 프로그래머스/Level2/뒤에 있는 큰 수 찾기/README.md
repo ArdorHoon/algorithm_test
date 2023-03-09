e9ad7a0f1eb36158a28274c19cf18e561ac7b8cd
ArdorHoon <didgnsah7320@gmail.com>
2023-03-09 16:00:50 +0900

    add lv2 solve

---
+++ 프로그래머스/Level2/뒤에 있는 큰 수 찾기/README.md
@@ -0,0 +1,8 @@
+# 뒤에 있는 큰 수 찾기
+
+
+[문제 페이지](https://school.programmers.co.kr/learn/courses/30/lessons/154539)
+
+
+* Stack
+* 시간 복잡도 고려
---
+++ 프로그래머스/Level2/뒤에 있는 큰 수 찾기/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(0);
+        for (int i = 1; i < numbers.length; i++) {
+
+            while (!stack.isEmpty()) {
+                int idx = stack.pop();
+
+                if (numbers[i] > numbers[idx]) {
+                    answer[idx] = numbers[i];
+
+                } else {
+                    stack.push(idx);
+                    break;
+                }
+
+            }
+            stack.push(i);
+        }
+
+
+        return answer;
+    }
+}
